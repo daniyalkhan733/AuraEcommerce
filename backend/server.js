60c93af40de7da07b59c90765dba6d43ffebb60d
daniyal-p <daniyal733khan@gmail.com>
2026-06-08 21:24:41 +0530

    ip address mani[utlated

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,12 @@ app.use(cors()); // Simplest way to allow all origins temporarily for showcase
 app.use(express.json());
 
 // MongoDB Connection
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://daniyal733khan_db_user:ICo9I9WUnDJ8jo9X@cluster0.dc0fyen.mongodb.net/?appName=Cluster0';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://daniyal733khan_db_user:ICo9I9WUnDJ8jo9X@cluster0.dc0fyen.mongodb.net/aura-ecommerce?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB connected successfully to:', mongoURI.split('@')[1]))
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
 const productRoutes = require('./routes/product.routes');
 const cartRoutes = require('./routes/cart.routes');
